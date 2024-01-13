ac63ece9184c084b1f906a3883f0e16eb677e74d
Rahmanbayramov <bayramovrahman9@gmail.com>
2024-01-13 23:10:51 +0300

    tel

--- js/script.js
+++ js/script.js
@@ -15,30 +15,6 @@ for (let smoothLink of smoothLinks) {
 // plavnoe
 
 
-const nav= document.getElementById('nav-icon2'),
-      ul = document.querySelector('.drop');
-      nav.addEventListener('click', () =>{
-    nav.classList.toggle('open');
-    ul.classList.toggle('none');})
-
-// karusel
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    // slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4500); // Change image every 2 seconds
-}
-
-
-
 
 
 
